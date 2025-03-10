fbc1c5f1bac117103a0d9e0e86aa4f7d1f0a6304
Anthony Fu <github@antfu.me>
2025-03-11 00:36:04 +0800

    chore: wip

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
+import type { App as H3App } from 'h3'
 import { createServer } from 'node:http'
 import process from 'node:process'
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js'
-import { createApp, createError, eventHandler, toNodeListener } from 'h3'
+import { createApp, createError, eventHandler, getQuery, toNodeListener } from 'h3'
 import { z } from 'zod'
+import { version } from '../package.json'
 
 const server = new McpServer(
   {
-    name: 'mcp-starter',
-    version: '0.1.0',
+    name: 'nuxt-mcp',
+    version,
   },
   {
     capabilities: {
@@ -46,34 +48,61 @@ server.resource(
   }),
 )
 
-process.on('SIGINT', async () => {
-  await server.close()
-  process.exit(0)
-})
+export function createMcpH3App(server: McpServer): H3App {
+  const app = createApp({
+    onError(error, event) {
+      console.error('Failed to handle event', event)
+      console.error(error)
+    },
+  })
 
-const app = createApp()
+  // Map to store client-specific transports
+  const clientTransports = new Map<string, SSEServerTransport>()
 
-let transport: SSEServerTransport
+  app.use('/sse', eventHandler(async (event) => {
+    const res = event.node.res
 
-app.use('/sse', eventHandler(async (event) => {
-  const res = event.node.res
-  transport = new SSEServerTransport('/messages', res)
-  await server.connect(transport)
-}))
-
-app.use('/messages', eventHandler(async (event) => {
-  if (event.method !== 'POST') {
-    throw createError({
-      statusCode: 405,
-      statusMessage: 'Method Not Allowed',
+    const transport = new SSEServerTransport('../messages', res)
+    clientTransports.set(transport.sessionId, transport)
+    res.on('close', () => {
+      clientTransports.delete(transport.sessionId)
     })
-  }
+    await server.connect(transport)
+  }))
 
-  // Note: to support multiple simultaneous connections, these messages will
-  // need to be routed to a specific matching transport. (This logic isn't
-  // implemented here, for simplicity.)
-  await transport?.handlePostMessage(event.node.req, event.node.res)
-}))
+  app.use('/messages', eventHandler(async (event) => {
+    if (event.method !== 'POST') {
+      throw createError({
+        statusCode: 405,
+        statusMessage: 'Method Not Allowed',
+      })
+    }
+
+    const clientId = event.node.req.headers['x-session-id']
+      || getQuery(event).sessionId
+
+    if (!clientId || typeof clientId !== 'string') {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Client ID not provided',
+      })
+    }
+
+    const transport = clientTransports.get(clientId)
+    if (!transport) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: 'Transport not found',
+      })
+    }
+
+    await transport.handlePostMessage(event.node.req, event.node.res)
+  }))
+
+  return app
+}
+
+const app = createMcpH3App(server)
 
 // Start the server
 createServer(toNodeListener(app))
@@ -81,3 +110,8 @@ createServer(toNodeListener(app))
     // eslint-disable-next-line no-console
     console.log('Server running on http://localhost:3001')
   })
+
+process.on('SIGINT', async () => {
+  await server.close()
+  process.exit(0)
+})
